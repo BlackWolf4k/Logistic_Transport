c46b135050b77fa4ee77dd9c51993930b0bd466c
AfzdCOLssl@protonmail.com <Riva.18361>
2022-11-08 11:43:05 +0100

    Fixed missing componend in Form1

--- Logistic_Transport/Form1.Designer.cs
+++ Logistic_Transport/Form1.Designer.cs
@@ -36,7 +36,9 @@
             this.run_b = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.make_new_table_b = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // columns_l
@@ -116,16 +118,28 @@
             this.make_new_table_b.Text = "New";
             this.make_new_table_b.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(784, 353);
+            this.dataGridView1.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -140,5 +154,6 @@
         private Button run_b;
         private Panel panel1;
         private Button make_new_table_b;
+        private DataGridView dataGridView1;
     }
 }
